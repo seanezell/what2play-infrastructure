00aee971f1c9285c0dfd3ec2f0faa2b193107202
Sean <162363+seanezell@users.noreply.github.com>
2025-12-15 23:49:44 -0800

    fix the github action role

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:seanezell/what2play-infrastructure:ref:refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "repo:seanezell/what2play-*:ref:refs/heads/*"
           }
         }
       }
