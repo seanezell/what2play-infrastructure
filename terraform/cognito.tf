2eabc44c29438d6cf2cdbd3ca671aeb070421cca
Sean <162363+seanezell@users.noreply.github.com>
2026-01-13 16:52:38 -0800

    trying to get create account working

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -7,7 +7,7 @@ resource "aws_cognito_user_pool" "what2play_cognito_userpool" {
     minimum_length                   = 8
     require_lowercase                = true
     require_numbers                  = true
-    require_symbols                  = true
+    require_symbols                  = false
     require_uppercase                = true
     temporary_password_validity_days = 7
   }
@@ -40,4 +40,6 @@ resource "aws_cognito_user_pool_client" "what2play_cognito_userpool_client" {
   allowed_oauth_flows                  = ["code"]
   allowed_oauth_scopes                 = ["email", "openid", "profile"]
   prevent_user_existence_errors        = "ENABLED"
+  read_attributes                      = ["email", "email_verified"]
+  write_attributes                     = ["email"]
 }
