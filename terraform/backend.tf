ac9990fd79449ca10235c8e7918ef5373363ffad
Sean <162363+seanezell@users.noreply.github.com>
2026-03-24 14:54:11 -0700

    chore: update terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,19 @@
 terraform {
+  required_version = ">= 1.14.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "seanezell-terraform-backend"
     key            = "what2play-infrastructure/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "terraform_state"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
